cfdd7ad6ff603ba335f0e8daba36bf8e480c3014
wholenote <brianwang2018@gmail.com>
2018-10-13 02:41:11 -0500

    gamedraft2
    added a animated bee and bug fixes.

---
+++ InquisitiveJohnGame/Assets/startingcamera.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class startingcamera : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        transform.position = new Vector3((float)-4.47, 0, (float)-10);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ InquisitiveJohnGame/Assets/startinglocation.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class startinglocation : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        transform.position = new Vector3((float)-8.16, (float)-2.87, 0);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
