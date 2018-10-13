370657f78a63a163c5db3ff8a19bbdec04d891f4
wholenote <brianwang2018@gmail.com>
2018-10-13 01:23:54 -0500

    gamedraft1
    added player with movement, added in banana coin animation

---
+++ InquisitiveJohnGame/Assets/movement.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class movement : MonoBehaviour {
+
+    public CharacterController2D controller;
+
+    public float runSpeed = 40f;
+
+    float horizontalMovement = 0f;
+    bool jump = false;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        horizontalMovement = Input.GetAxisRaw("Horizontal") * runSpeed;
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            jump = true;
+        }
+	}
+
+    void FixedUpdate()
+    {
+        controller.Move(horizontalMovement * Time.fixedDeltaTime, false, jump);
+        jump = false;
+    }
+}
